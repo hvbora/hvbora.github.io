76fde8fe15aeaf9f9eb30fff0dd54a7f82835c37
hvbora <hvbora@gmail.com>
2022-12-29 20:01:20 +0530

    2022 final update

--- js/custom.js
+++ js/custom.js
@@ -5,8 +5,7 @@ $(function(){
 		$('body').backstretch([
        "images/tm-bg-slide-1.jpg",
        "images/tm-bg-slide-2.jpg",
-	"images/tm-bg-slide-3.jpg",
-       "images/tm-bg-slide-3(copy).jpg"
+	     "images/tm-bg-slide-3.jpg"
      ], 	{duration: 7200, fade: 2300});
 		});
 })
